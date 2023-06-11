3eaf6b29adf0e062460ced62fb1a7291b0f92f7c
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-06-11 11:53:30 +0200

    nav changes

--- bootstrap_app/js/functions.js
+++ bootstrap_app/js/functions.js
@@ -8,5 +8,6 @@ function add_toggle()
     if (window.innerWidth <= 576)
     {
         document.getElementById("fractions").setAttribute("data-bs-toggle", "dropdown");
+        document.getElementById("characters").setAttribute("data-bs-toggle", "dropdown");
     }
 }
