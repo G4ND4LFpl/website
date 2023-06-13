fbcaf9edb52fe51484eee199311a34fde718a7fa
G4ND4LFpl <71659952+G4ND4LFpl@users.noreply.github.com>
2023-06-13 22:23:50 +0200

    new changes

--- bootstrap_app/js/functions.js
+++ bootstrap_app/js/functions.js
@@ -2,6 +2,36 @@ function OnLoadResponse()
 {
     add_toggle();
 }
+function changeLang()
+{
+    var val = document.getElementById("lang_selection_form").value;
+    var data;
+
+    switch(val)
+    {
+        case "1":
+            {
+                data = fetch("lang/pl.json").then(response => response.json())
+                break;
+            }
+        case "2":
+            {
+                data = fetch("lang/en.json").then(response => response.json())
+                break;
+            }
+        /*case "3":
+            {
+                let data = fetch("es.json")
+                break;
+            } */
+        default:
+            {
+                data = fetch("lang/pl.json").then(response => response.json())
+            }
+    }
+
+    console.log(data);
+}
 
 function add_toggle()
 {
@@ -9,5 +39,7 @@ function add_toggle()
     {
         document.getElementById("fractions").setAttribute("data-bs-toggle", "dropdown");
         document.getElementById("characters").setAttribute("data-bs-toggle", "dropdown");
+        document.getElementById("vehicles").setAttribute("data-bs-toggle", "dropdown");
+        document.getElementById("worlds").setAttribute("data-bs-toggle", "dropdown");
     }
 }
